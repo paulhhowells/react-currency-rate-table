40b54243f68cb294b9528c339e185b7fc1d81638
paulhhowells <p.h.howells@gmail.com>
2019-12-11 21:59:30 +0000

    refactor: memoized DataRow

--- src/components/CurrencyRateTable/components.js
+++ src/components/CurrencyRateTable/components.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const DataRow = ({ row, ...props }) => (
+export const DataRow = React.memo(({ row, ...props }) => (
   <tr {...props}>
     <td key={ row.currency + 0 }>{ row.currency }</td>
     <td key={ row.currency + 1 }>{ row.rate }</td>
   </tr>
-);
+));
 
 DataRow.propTypes = {
   row: PropTypes.object.isRequired,
